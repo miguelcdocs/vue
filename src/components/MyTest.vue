b48afcc92b27d81b5f32ae96c48926a964fdbf8d
Miguel <miguelcdoc@gmail.com>
2019-04-30 10:42:00 +0200

    first commit vue project

---
+++ src/components/MyTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'MyTest',
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='home'>
+    <img alt='Vue logo' src='../assets/logo.png'>
+    <MyComponent msg='Learning...'></MyComponent>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyComponent from '@/components/MyTest.vue';
+
+export default {
+  name: 'home',
+  components: {
+    MyComponent,
+  },
+};
+</script>
